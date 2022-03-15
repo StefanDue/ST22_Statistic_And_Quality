3968cf902d7493df949517e4a563d1a8c53e76f6
StefanDue <stefan.duenser@students.fhv.at>
2022-03-15 14:20:30 +0100

    Erste Uebung angefangen

---
+++ RStudio_Introduction/FirstStepsWith_R.R
@@ -0,0 +1,9 @@
+# --------------------------------------------------
+# Term    WT22, ET-Dual
+# Purpos  First steps with R
+# Author  SteDun00
+# Date    2022-03-08
+# --------------------------------------------------
+
+print("This are the first steps of working with R")
+17 + 4
